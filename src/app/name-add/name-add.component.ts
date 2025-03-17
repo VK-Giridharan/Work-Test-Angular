27cbe25d5af5561a668d0eb7e6cae9b08129fe32
VK-Giridharan <giridharan.vk.developer@gmail.com>
2025-03-17 18:04:31 +0530

    Inpute Validation

--- src/app/name-add/name-add.component.ts
+++ src/app/name-add/name-add.component.ts
@@ -1,18 +1,24 @@
 import { Component } from '@angular/core';
 import { NameService } from '../services/name.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-name-add',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './name-add.component.html',
   styleUrl: './name-add.component.css'
 })
 export class NameAddComponent {
   names:string[]=[];
+  errormessage:string="";
   constructor(private nameservice:NameService){}
  
 addname(name:string){
+  if (!name.trim()){
+    this.errormessage='Name canot be empty';
+    return;
+  }
    this.nameservice.add(name);
-}
-
+   this.errormessage='';
+  }
 }
